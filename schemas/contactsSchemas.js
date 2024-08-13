17a3ebb4895d83343c5bd45e58f4a3c85e12287d
olita16 <119816415+olita16@users.noreply.github.com>
2024-08-13 10:33:23 +0200

    add function to controllers, schemas, services, app.js, create gitignore

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,19 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": "name has be exist" }),
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": "email has be exist" }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": "phone has be exist" }),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
