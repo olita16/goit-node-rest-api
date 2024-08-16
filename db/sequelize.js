63eeacd9611c25e642be1ff8ae1763539a73ed1c
olita16 <119816415+olita16@users.noreply.github.com>
2024-08-16 21:52:37 +0200

    add databaseConfig to config.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,15 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "olena",
-  database: "my_contacts_0x77",
-  password: "SxeDFYJnDYDuBvnaoKSlMKuWKcpJ2MCB",
-  host: "dpg-cqvpdsjtq21c7381g7kg-a.frankfurt-postgres.render.com",
-  port: "5432",
-  dialectOptions: {
-    ssl: true,
-  }
-});
+import { databaseConfig } from "./config";
+
+const sequelize = new Sequelize(databaseConfig);
 
 export default sequelize; 
